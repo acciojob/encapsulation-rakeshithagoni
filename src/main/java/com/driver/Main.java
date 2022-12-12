4a87fee3690d191eeb93461bf006de27c8a83a4a
Rakesh <rakeshgoudithagoni@gmail.com>
2022-12-12 21:23:40 +0530

    rock

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,10 @@
 package com.driver;
 
 public class Main {
-  
+  public static void mani(String[] args){
+      RWOnly obj =new RWOnly();
+      obj.setName("Rakesh");
+      System.out.println(obj.getName());
+  }
+
 }
